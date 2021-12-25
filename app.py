2b3bc562363d14940df2ecb92377697cd39b916d
Quentin <Quentin.Rivollat@etu.unige.ch>
2021-12-25 15:13:35 +0100

    DB configuration

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, url_for, request, redirect, make_response      
+from settings import *
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///budgetease.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 db = SQLAlchemy(app)
 
 #...
